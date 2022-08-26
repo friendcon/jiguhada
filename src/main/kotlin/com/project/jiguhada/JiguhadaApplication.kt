887fb6ffe2d771811488b733b91dccd3d53b95d4
hyyoo <imyhy01@gmail.com>
2022-08-26 19:37:01 +0900

    first commit

---
+++ src/main/kotlin/com/project/jiguhada/JiguhadaApplication.kt
@@ -0,0 +1,11 @@
+package com.project.jiguhada
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JiguhadaApplication
+
+fun main(args: Array<String>) {
+    runApplication<JiguhadaApplication>(*args)
+}
---
+++ src/test/kotlin/com/project/jiguhada/JiguhadaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.jiguhada
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JiguhadaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
