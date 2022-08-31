a6f7c6e6231b6898c0e8a5ba909a39e6226ab259
hyyoo <imyhy01@gmail.com>
2022-08-31 16:46:25 +0900

    update. JiguhadaApplicationTests activeProfile 추가

--- src/test/kotlin/com/project/jiguhada/JiguhadaApplicationTests.kt
+++ src/test/kotlin/com/project/jiguhada/JiguhadaApplicationTests.kt
@@ -2,10 +2,11 @@ package com.project.jiguhada
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class JiguhadaApplicationTests {
-
     @Test
     fun contextLoads() {
     }
