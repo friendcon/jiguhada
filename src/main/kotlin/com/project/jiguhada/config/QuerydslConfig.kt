911831decc44c5b3ed45e003b48ff6145f76c9f1
hyyoo <imyhy01@gmail.com>
2022-09-13 23:10:45 +0900

    add: querydsl 설정 추가

---
+++ src/main/kotlin/com/project/jiguhada/config/QuerydslConfig.kt
@@ -0,0 +1,17 @@
+package com.project.jiguhada.config
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+@Configuration
+class QuerydslConfig(
+    @PersistenceContext private val entityManager: EntityManager
+) {
+    @Bean
+    fun jpaQueryFactory(): JPAQueryFactory {
+        return JPAQueryFactory(this.entityManager)
+    }
+}
