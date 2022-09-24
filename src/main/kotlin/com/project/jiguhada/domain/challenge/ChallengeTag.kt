bbc30d4bd8d35b84f11b1cc75d1d80bf0071428d
hyyoo <imyhy01@gmail.com>
2022-09-24 22:40:32 +0900

    update: 챌린지 태그 도메인 설계 변경

--- src/main/kotlin/com/project/jiguhada/domain/challenge/ChallengeTag.kt
+++ src/main/kotlin/com/project/jiguhada/domain/challenge/ChallengeTag.kt
@@ -1,14 +1,29 @@
 package com.project.jiguhada.domain.challenge
 
-import com.project.jiguhada.util.CHALLENGE_TAG
-import javax.persistence.Entity
-import javax.persistence.EnumType
-import javax.persistence.Enumerated
-import javax.persistence.Id
+import org.hibernate.Hibernate
+import javax.persistence.*
 
 @Entity
 data class ChallengeTag(
     @Id
-    @Enumerated(EnumType.STRING)
-    val challengeTagName: CHALLENGE_TAG
-)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @ManyToOne
+    @JoinColumn(name = "tag_challenge_tag_name")
+    val tag: Tag
+) {
+
+    override fun toString(): String {
+        return "ChallengeTag(id=$id, tag=$tag)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as ChallengeTag
+
+        return id != null && id == other.id
+    }
+
+    override fun hashCode(): Int = javaClass.hashCode()
+}
--- src/main/kotlin/com/project/jiguhada/repository/challenge/ChallengeTagRepository.kt
+++ src/main/kotlin/com/project/jiguhada/repository/challenge/ChallengeTagRepository.kt
@@ -3,5 +3,5 @@ package com.project.jiguhada.repository.challenge
 import com.project.jiguhada.domain.challenge.ChallengeTag
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ChallengeTagRepository: JpaRepository<ChallengeTag, String> {
+interface ChallengeTagRepository: JpaRepository<ChallengeTag, Long> {
 }
