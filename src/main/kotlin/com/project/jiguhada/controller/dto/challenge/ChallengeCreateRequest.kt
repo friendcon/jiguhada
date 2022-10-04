539a5e040aa1e68fe03edb9c8df49cd47537092a
hyyoo <imyhy01@gmail.com>
2022-10-04 15:23:32 +0900

    update: 챌린지 , 챌린지 이미지 생성

--- src/main/kotlin/com/project/jiguhada/controller/dto/challenge/ChallengeCreateRequest.kt
+++ src/main/kotlin/com/project/jiguhada/controller/dto/challenge/ChallengeCreateRequest.kt
@@ -8,15 +8,19 @@ import java.time.LocalDate
 import java.time.LocalTime
 
 data class ChallengeCreateRequest(
-    val title: String,
-    val challengeDetails: String, // 챌린지 간단한 설명
-    val participantsCount: Long, // 참가자 수
     val challengeTag: List<CHALLENGE_TAG>,
+    val title: String, // 챌린지 제목
+    val challengeDetails: String, // 챌린지 간단한 설명
     val challengeImg: String, // 챌린지 대표 이미지
+    val challengeAddDetails: String, // 챌린지 추가 설명
+    val challengeAddImg: String , // 챌린지 추가 이미지
+    val participantsCount: Long, // 참가자 수
     val authMethodContent: String, // 인증방법 설명
-    val authMethodImg: String, // 인증방법 이미지
+    val authMethodImg: String, // 인증 성공 이미지
+    val authMethodFailImg: String, // 인증 실패 이미지
     val challengeStartDate: LocalDate, // 챌린지 시작일
     val challengePeroid: CHALLENGE_PERIOD, // 챌린지 기간 (1주, 2주, 3주, 4주)
+    val challengeEndDate: LocalDate, // 챌린지 종료일
     val authFrequency: AUTH_FREQUENCY, // 인증빈도
     val authCountPerDay: Long, // 하루 인증 횟수
     val authAvailableTimeType: AUTH_AVAILABLE_TIME_TYPE, // 인증 가능 시간 타입 (24시간, 지정한 시간)
--- src/main/kotlin/com/project/jiguhada/service/ChallengeService.kt
+++ src/main/kotlin/com/project/jiguhada/service/ChallengeService.kt
@@ -3,6 +3,7 @@ package com.project.jiguhada.service
 import com.project.jiguhada.controller.dto.challenge.ChallengeCreateRequest
 import com.project.jiguhada.controller.dto.challenge.ChallengeCreateResponse
 import com.project.jiguhada.controller.dto.challenge.ChallengeJoinRequest
+import com.project.jiguhada.controller.dto.user.ImgUrlResponseDto
 import com.project.jiguhada.domain.challenge.Challenge
 import com.project.jiguhada.domain.challenge.ChallengeTag
 import com.project.jiguhada.domain.challenge.UserChallenge
@@ -10,46 +11,63 @@ import com.project.jiguhada.exception.ChallengeJoinCountException
 import com.project.jiguhada.exception.ChallengeJoinEndException
 import com.project.jiguhada.exception.UserAlreadyChallengeMemberException
 import com.project.jiguhada.repository.challenge.ChallengeRepository
-import com.project.jiguhada.repository.challenge.ChallengeTagRepository
 import com.project.jiguhada.repository.challenge.TagRepository
 import com.project.jiguhada.repository.challenge.UserChallengeRepository
 import com.project.jiguhada.repository.user.UserEntityRepository
-import com.project.jiguhada.util.*
-import org.springframework.data.domain.Pageable
+import com.project.jiguhada.util.CHALLENGE_PERIOD
+import com.project.jiguhada.util.CHALLENGE_STATUS
+import com.project.jiguhada.util.SecurityUtil
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.multipart.MultipartFile
 import java.math.BigDecimal
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
 
 @Service
 class ChallengeService(
     private val userEntityRepository: UserEntityRepository,
     private val challengeRepository: ChallengeRepository,
-    private val challengeTagRepository: ChallengeTagRepository,
+    private val awsS3Service: AwsS3Service,
     private val userChallengeRepository: UserChallengeRepository,
     private val tagRepository: TagRepository
 ) {
+    companion object {
+        const val CHALLENGE_PROFILE_DIR_NAME = "challenge-profile-img"
+    }
     @Transactional
     fun createChallenge(challengeCreateRequest: ChallengeCreateRequest): ChallengeCreateResponse {
         val challenge = challengeRepository.save(challengeCreateRequest.toEntity())
+
         val user = userEntityRepository.findByUsername(SecurityUtil.currentUsername)
         val userChallenge = UserChallenge(
             userEntity = user.get(),
             challenge = challenge,
             achievementRate = BigDecimal.valueOf(0.00)
         )
+
         userChallengeRepository.save(userChallenge)
+
         return challenge.toChallengeCreateResponse()
     }
 
+    @Transactional
+    fun uploadChallengeImg(multipartFile: MultipartFile): ImgUrlResponseDto {
+        val url = awsS3Service.uploadImgToDir(multipartFile, CHALLENGE_PROFILE_DIR_NAME)
+        return ImgUrlResponseDto(url)
+    }
+
+
+    // 챌린지 참여
     @Transactional
     fun joinChallenge(challengeJoinRequest: ChallengeJoinRequest): ChallengeCreateResponse {
         val userList = userChallengeRepository.findByChallengeId(challengeJoinRequest.challengeId).map { it.userEntity.id }
         val challenge = challengeRepository.findById(challengeJoinRequest.challengeId).get()
         val user = userEntityRepository.findByUsername(SecurityUtil.currentUsername).get()
 
-        if(LocalDate.now().isAfter(challenge.challengeStartDate) ||
-            LocalDate.now().isEqual(challenge.challengeStartDate)) {
+        if(LocalDateTime.now().isAfter(challenge.challengeStartDate) ||
+            LocalDateTime.now().isEqual(challenge.challengeStartDate)) {
             throw ChallengeJoinEndException("챌린지 모집기간이 지났습니다.")
         } else if(challenge.currrentParticipantsCount == challenge.participantsCount) {
             throw ChallengeJoinCountException("정원이 초과되어 챌린지에 참여할 수 없습니다.")
@@ -68,33 +86,25 @@ class ChallengeService(
         return challenge.toChallengeCreateResponse()
     }
 
-    @Transactional
-    fun readChallengeList(
-        query: String?,
-        searchType: CHALLEGE_SEARCH_TYPE,
-        orderType: CHALLENGE_ORDER_TYPE?,
-        challengeTag: List<CHALLENGE_TAG>?,
-        page: Pageable
-    ) {
-        
-    }
-
     fun ChallengeCreateRequest.toEntity(): Challenge {
         val user = userEntityRepository.findByUsername(SecurityUtil.currentUsername).get()
 
         return Challenge(
+            challengeTags = challengeTag.map { ChallengeTag(tag = tagRepository.findByChallengeTagName(it)) },
             title = title,
-            userEntity = user,
             challengeDetails = challengeDetails,
+            challengeImg = challengeImg, // 챌린지 대표 사진
+            challengeAddDetails = challengeDetails,
+            challengeAddImg = challengeAddImg, // 챌린지 추가 이미지
+            userEntity = user,
             participantsCount = participantsCount,
             currrentParticipantsCount = 1,
-            challengeTags = challengeTag.map { ChallengeTag(tag = tagRepository.findByChallengeTagName(it)) },
-            challengeImg = challengeImg,
             authMethodContent = authMethodContent,
             authMethodImgUrl = authMethodImg,
-            challengeStartDate = challengeStartDate,
+            authMethodFailImg = authMethodFailImg,
+            challengeStartDate = LocalDateTime.of(challengeStartDate, LocalTime.of(0,0,0)),
+            challengePeroid = challengePeroid, // 챌린지 기간
             challengeEndDate = calculateChallengeEndDate(challengeStartDate, challengePeroid),
-            challengePeroid = challengePeroid,
             authFrequency = authFrequency,
             authCountPerDay = authCountPerDay,
             authAvailableTimeType = authAvailableTimeType,
@@ -107,12 +117,13 @@ class ChallengeService(
         )
     }
 
-    fun calculateChallengeEndDate(startDate: LocalDate, challengePeriod: CHALLENGE_PERIOD): LocalDate {
+    fun calculateChallengeEndDate(startDate: LocalDate, challengePeriod: CHALLENGE_PERIOD): LocalDateTime {
+        val endTime = LocalTime.of(23,59,59,999999999)
         return when(challengePeriod) {
-            CHALLENGE_PERIOD.ONEWEEK -> startDate.plusWeeks(1)
-            CHALLENGE_PERIOD.TWOWEEK -> startDate.plusWeeks(2)
-            CHALLENGE_PERIOD.THREEWEEK -> startDate.plusWeeks(3)
-            CHALLENGE_PERIOD.FOURWEEK -> startDate.plusWeeks(4)
+            CHALLENGE_PERIOD.ONEWEEK -> LocalDateTime.of(startDate, endTime).plusWeeks(1)
+            CHALLENGE_PERIOD.TWOWEEK -> LocalDateTime.of(startDate, endTime).plusWeeks(2)
+            CHALLENGE_PERIOD.THREEWEEK -> LocalDateTime.of(startDate, endTime).plusWeeks(3)
+            CHALLENGE_PERIOD.FOURWEEK -> LocalDateTime.of(startDate, endTime).plusWeeks(4)
         }
     }
 }
